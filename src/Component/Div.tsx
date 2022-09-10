2d49663e9c0beed8a5d32155c67816993ef7a942
Lucas Rivoiro <lucasrivoiro@gmail.com>
2022-09-09 22:38:58 -0300

    Implemented common listeners and props for P and Div components

--- src/Component/Div.tsx
+++ src/Component/Div.tsx
@@ -1,12 +1,45 @@
 import { useBlock } from 'Hook/Style';
 import React from 'react';
+import { cx } from 'utils';
 
 import { BlockInterface } from 'Interface/Styles';
 
-const Div = ({ children, ...props }: BlockInterface): JSX.Element => {
+const Div = ({
+  children,
+  className,
+  contentEditable,
+  onBlur,
+  onChange,
+  onClick,
+  onMouseDown,
+  onMouseEnter,
+  onMouseLeave,
+  onMouseMove,
+  onMouseOut,
+  onMouseOver,
+  onMouseUp,
+  ...props
+}: BlockInterface): JSX.Element => {
   const classes = useBlock(props);
 
-  return <div className={classes}>{children}</div>;
+  return (
+    <div
+      className={cx(classes, className)}
+      contentEditable={contentEditable}
+      onBlur={onBlur}
+      onChange={onChange}
+      onClick={onClick}
+      onMouseDown={onMouseDown}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      onMouseMove={onMouseMove}
+      onMouseOut={onMouseOut}
+      onMouseOver={onMouseOver}
+      onMouseUp={onMouseUp}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Div;
--- src/Component/P.tsx
+++ src/Component/P.tsx
@@ -1,12 +1,45 @@
 import { useText } from 'Hook/Style';
 import React from 'react';
+import { cx } from 'utils';
 
 import { TextInterface } from 'Interface/Styles';
 
-const P = ({ children, ...props }: TextInterface): JSX.Element => {
+const P = ({
+  children,
+  className,
+  contentEditable,
+  onBlur,
+  onChange,
+  onClick,
+  onMouseDown,
+  onMouseEnter,
+  onMouseLeave,
+  onMouseMove,
+  onMouseOut,
+  onMouseOver,
+  onMouseUp,
+  ...props
+}: TextInterface): JSX.Element => {
   const classes = useText(props);
 
-  return <p className={classes}>{children}</p>;
+  return (
+    <p
+      className={cx(classes, className)}
+      contentEditable={contentEditable}
+      onBlur={onBlur}
+      onChange={onChange}
+      onClick={onClick}
+      onMouseDown={onMouseDown}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      onMouseMove={onMouseMove}
+      onMouseOut={onMouseOut}
+      onMouseOver={onMouseOver}
+      onMouseUp={onMouseUp}
+    >
+      {children}
+    </p>
+  );
 };
 
 export default P;
