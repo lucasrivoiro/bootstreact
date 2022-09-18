d9b215e58ce6d6b9b6f61eacdc6e6d86035d7854
Lucas Rivoiro <lucasrivoiro@gmail.com>
2022-09-18 00:36:28 -0300

    Added AppendBody component

---
+++ src/Component/AppendBody.tsx
@@ -0,0 +1,24 @@
+import { createPortal } from 'react-dom';
+
+let container = document.getElementById('appended-elements');
+
+if (!container) {
+  const body = document.querySelectorAll('body')[0];
+
+  const div = document.createElement('div');
+  div.id = 'appended-elements';
+
+  body.append(div);
+
+  container = document.getElementById('appended-elements');
+}
+
+interface Props {
+  children: JSX.Element;
+}
+
+const AppendBody = ({ children }: Props): JSX.Element => {
+  return createPortal(children, container as HTMLElement);
+};
+
+export default AppendBody;
