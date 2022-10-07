4565eca1112adcc508b85caf06abd4c75adc38ca
Johnny Sprone <johnnysprone@hotmail.com>
2022-10-07 12:17:27 -0300

    Adjusted offcanvas component to use bootstrap native class

--- src/Component/Offcanvas.tsx
+++ src/Component/Offcanvas.tsx
@@ -1,7 +1,11 @@
+import { Offcanvas as BsOffcanvas } from 'bootstrap';
 import React, {
   createContext,
   useContext,
+  useEffect,
   useLayoutEffect,
+  useMemo,
+  useRef,
   useState,
 } from 'react';
 import { cn } from 'utils';
@@ -9,10 +13,18 @@ import { cn } from 'utils';
 import AppendToBody from './AppendToBody';
 
 interface OffcanvasProps {
+  backdrop?: boolean | 'static';
   children: JSX.Element | JSX.Element[];
+  keyboard?: boolean;
+  lg?: boolean;
+  md?: boolean;
   onClose: (state: boolean) => void;
-  show: boolean;
   placement?: 'start' | 'end' | 'top' | 'bottom';
+  scroll?: boolean;
+  show: boolean;
+  sm?: boolean;
+  xl?: boolean;
+  xxl?: boolean;
 }
 
 interface HeaderProps {
@@ -20,7 +32,7 @@ interface HeaderProps {
 }
 
 interface BodyProps {
-  children: JSX.Element | JSX.Element[];
+  children: JSX.Element | JSX.Element[] | string | string[];
 }
 
 interface ContextInterface {
@@ -32,12 +44,57 @@ let numberOfOffcanvas = 0;
 const Context = createContext<ContextInterface>({});
 
 const Offcanvas = ({
+  backdrop = true,
   children,
+  keyboard = true,
+  lg,
+  md,
   onClose,
+  placement = 'start',
+  scroll = false,
   show = false,
-  placement = 'end',
+  sm,
+  xl,
+  xxl,
 }: OffcanvasProps): JSX.Element => {
+  const ref = useRef<HTMLDivElement>(null);
   const [zIndex, setZIndex] = useState<number>(0);
+
+  const offcanvas = useMemo(() => {
+    if (ref.current) {
+      ref.current.addEventListener('hide.bs.offcanvas', () => {
+        onClose(false);
+      });
+
+      return new BsOffcanvas(ref.current, {
+        backdrop,
+        scroll,
+        keyboard,
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [backdrop, scroll, keyboard, ref.current]);
+
+  useEffect(() => {
+    if (offcanvas) {
+      if (show) {
+        offcanvas.show();
+
+        if (backdrop) {
+          const backdropElement = document.querySelectorAll<HTMLDivElement>(
+            'div.offcanvas-backdrop:last-child'
+          );
+
+          if (backdropElement.length) {
+            backdropElement[0].style.zIndex = (1055 + zIndex).toString();
+          }
+        }
+      } else {
+        offcanvas.hide();
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [offcanvas, show]);
 
   useLayoutEffect(() => {
     numberOfOffcanvas++;
@@ -45,25 +102,23 @@ const Offcanvas = ({
   }, []);
 
   return (
-    <React.Fragment>
-      {show && (
-        <AppendToBody>
-          <Context.Provider value={{ onClose }}>
-            <div
-              className={cn('offcanvas', `offcanvas-${placement}`, 'show')}
-              style={{ zIndex: 1045 + zIndex }}
-            >
-              {children}
-            </div>
-            <div
-              className="offcanvas-backdrop fade show"
-              onClick={() => onClose(false)}
-              style={{ zIndex: 1040 + zIndex }}
-            ></div>
-          </Context.Provider>
-        </AppendToBody>
-      )}
-    </React.Fragment>
+    <AppendToBody>
+      <Context.Provider value={{ onClose }}>
+      <div
+        ref={ref}
+        className={cn('offcanvas', `offcanvas-${placement}`, {
+          'offcanvas-xxl': xxl,
+          'offcanvas-xl': xl,
+          'offcanvas-lg': lg,
+          'offcanvas-md': md,
+          'offcanvas-sm': sm,
+        })}
+        style={{ zIndex: 1065 + zIndex }}
+      >
+        {children}
+      </div>
+      </Context.Provider>
+    </AppendToBody>
   );
 };
 
@@ -71,16 +126,16 @@ const Header = ({ children }: HeaderProps): JSX.Element => {
   const { onClose } = useContext(Context);
 
   return (
-    <div className="offcanvas-header">
-      <h5 className="offcanvas-title">{children}</h5>
-      {onClose && (
-        <button
-          type="button"
-          className="btn-close"
-          onClick={() => onClose(false)}
-        ></button>
-      )}
-    </div>
+  <div className="offcanvas-header">
+    <h5 className="offcanvas-title">{children}</h5>
+    {onClose && (
+      <button
+        type="button"
+        className="btn-close"
+        onClick={() => onClose(false)}
+      ></button>
+    )}
+  </div>
   );
 };
 
@@ -92,3 +147,4 @@ Offcanvas.Header = Header;
 Offcanvas.Body = Body;
 
 export default Offcanvas;
+  
