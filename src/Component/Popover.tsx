05e7437a7496e32bb4f6e77477a5743d6ea4dc35
Johnny Sprone <johnnysprone@hotmail.com>
2022-10-07 12:19:41 -0300

    Added Popover component

---
+++ src/Component/Popover.tsx
@@ -0,0 +1,42 @@
+import { Popover as BsPopover } from 'bootstrap';
+import React, { useEffect, useRef } from 'react';
+
+interface Props {
+  children?: JSX.Element;
+  content: string;
+  title: string;
+  placement?: 'top' | 'bottom' | 'right' | 'left';
+  trigger?:
+    | 'click'
+    | 'hover'
+    | 'focus'
+    | 'manual'
+    | 'click hover'
+    | 'click focus'
+    | 'hover focus'
+    | 'click hover focus';
+}
+
+export const Popover = ({
+  children,
+  content,
+  placement = 'top',
+  trigger = 'focus',
+  title,
+}: Props) => {
+  const ref = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      const t = new BsPopover(ref.current, {
+        content,
+        placement,
+        title,
+        trigger,
+      });
+      return () => t.dispose();
+    }
+  }, [content, placement, title, trigger]);
+
+  return <span ref={ref}>{children}</span>;
+};
