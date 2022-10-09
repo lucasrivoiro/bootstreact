2bfa5f84dcc48a434a63194da82b208917e1d92e
Lucas Rivoiro <lucasrivoiro@gmail.com>
2022-10-09 18:15:54 -0300

    Added cols property in row component

--- src/Component/Row.tsx
+++ src/Component/Row.tsx
@@ -4,17 +4,48 @@ import { cn } from 'utils';
 
 import { ClassesInterface } from 'Interface/Styles';
 
+type Cols = 1 | 2 | 3 | 4;
+
 interface Props extends ClassesInterface {
   children?: JSX.Element | JSX.Element[] | string | string[];
   className?: string | undefined;
+  cols?: Cols;
+  colsSm?: Cols;
+  colsMd?: Cols;
+  colsLg?: Cols;
+  colsXl?: Cols;
+  colsXxl?: Cols;
 }
 
-const Row = ({ children, className, ...props }: Props): JSX.Element => {
+const Row = ({
+  children,
+  className,
+  cols,
+  colsSm,
+  colsMd,
+  colsLg,
+  colsXl,
+  colsXxl,
+  ...props
+}: Props): JSX.Element => {
   const propsClasses = useClasses(props);
 
   const classes = useMemo(
-    () => cn('row', propsClasses, className),
-    [className, propsClasses]
+    () =>
+      cn(
+        'row',
+        {
+          [`row-cols-${cols}`]: cols,
+          [`row-cols-sm-${cols}`]: colsSm,
+          [`row-cols-md-${cols}`]: colsMd,
+          [`row-cols-lg-${cols}`]: colsLg,
+          [`row-cols-xl-${cols}`]: colsXl,
+          [`row-cols-xxl-${cols}`]: colsXxl,
+        },
+        propsClasses,
+        className
+      ),
+    [className, cols, colsLg, colsMd, colsSm, colsXl, colsXxl, propsClasses]
   );
 
   return <div className={classes}>{children}</div>;
